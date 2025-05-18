0bf0650ea498146b37ed982de74b3f951b3cec9c
liwezee <63093684+liwezee@users.noreply.github.com>
2025-05-18 21:07:36 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@
       <span class="anthor" @click="jumpTo(copyrightLink ?? 'https://github.com/liwezee/Snavigation')">
         {{ siteAnthor }}
       </span>
-      <span v-if="icp" class="icp" @click="jumpTo('https://beian.miit.gov.cn')">
+      <span v-if="icp" class="icp" @click="jumpTo('https://icp.gov.moe/?keyword=20222216')">
         {{ icp }}
       </span>
       <span class="about" @click="aboutSiteModal = true">关于</span>
